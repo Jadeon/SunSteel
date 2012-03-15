6cc1423310cf45d832d7acec8a7460f55c4ae732
JD Davenport <acyrid@gmail.com>
2012-03-15 16:05:13 -0500

    Optimizations & Clean Up Added 'special' perm domain for handling effects

--- src/main/java/com/acyrid/SunSteel/utils/SSPermissions.java
+++ src/main/java/com/acyrid/SunSteel/utils/SSPermissions.java
@@ -12,37 +12,47 @@ public class SSPermissions{
     private static final String bootsPerm = "sunsteel.armor.boots";
     private static final String helmsPerm = "sunsteel.armor.helm";
     private static final String pantsPerm = "sunsteel.armor.legs";
-    private static final String hoverLavaPerm = "sunsteel.armor.boots.lavafloat";
-    private static final String hoverWaterPerm = "sunsteel.armor.boots.waterfloat";
+    private static final String hoverLavaPerm = "sunsteel.armor.special.lavafloat";
+    private static final String hoverWaterPerm = "sunsteel.armor.special.waterfloat";
+    //private static final String breatheLavaPerm = "sunsteel.armor.special.lavabreath";
+    private static final String breatheWaterPerm = "sunsteel.armor.special.waterbreath";
     private static final String swordPerm = "sunsteel.tools.sword";
     private static final String axePerm = "sunsteel.tools.axe";
     private static final String pickPerm = "sunsteel.tools.pick";
     private static final String shovelPerm = "sunsteel.tools.shovel";
     private static final String adminPerm = "sunsteel.admins";
 
-    //Armor Child Perm: Chest
+
     public static boolean allowedChest(Player player){
         return player.hasPermission(chestPerm);
     }
-    //Armor Child Perm: Legs
+
     public static boolean allowedPants(Player player){
         return player.hasPermission(pantsPerm);
     }
-    //Armor Child Perm: Helm
+
     public static boolean allowedHelm(Player player){
         return player.hasPermission(helmsPerm);
     }
-    //Armor Child Perm: Boots
+
     public static boolean allowedBoots(Player player){
         return player.hasPermission(bootsPerm);
     }
-    //Armor.Boots Child Perm: Hover
+
     public static boolean allowedLavaHover(Player player){
         return player.hasPermission(hoverLavaPerm);
     }
 
     public static boolean allowedWaterHover(Player player){
         return player.hasPermission(hoverWaterPerm);
+    }
+
+   /* public static boolean allowedLavaBreath(Player player){
+        return player.hasPermission(breatheLavaPerm);
+    }*/
+
+    public static boolean allowedWaterBreath(Player player){
+        return player.hasPermission(breatheWaterPerm);
     }
 
     public static boolean allowedSword(Player player){
