54cad0f9b20514a654445415cc32efd330de2849
JD Davenport <acyrid@gmail.com>
2012-03-15 12:53:22 -0500

    Clean up of declaration references

--- src/main/java/com/acyrid/SunSteel/utils/SSConfig.java
+++ src/main/java/com/acyrid/SunSteel/utils/SSConfig.java
@@ -1,36 +1,18 @@
 package com.acyrid.SunSteel.utils;
 
-import org.bukkit.entity.Animals;
-
-import java.io.FileInputStream;
-import java.util.ArrayList;
-
-
 public class SSConfig {
-    private static String configPath = "plugins/FireLord/config.properties";
-    private static String configFile = "config.properties";
-    static int fireDuration = 40;
-    private static int fireReflectDuration = 40;
-    static int swordId = 283;
-    static int helmId = 314;
-    static int chestId = 315;
-    static int bootsId = 317;
-    static int legsId = 316;
-    static int pickId = 285;
-    static int axeId = 286;
-    static int shovelId = 284;
-    private static boolean fireSword = true;
-    private static boolean firePick = true;
-    private static boolean fireAxe = true;
-    private static boolean fireShovel = true;
-    static boolean fireResist = true;
-    static boolean fireRiposte = true;
-    static boolean fireBreather = false;
-    static boolean waterWalker = false;
-    static boolean lavaWalker = false;
-    private static boolean allowedPvp = true;
-    static int hitChance = 100;
-    static int smeltChance = 100;
-    static boolean isAllowedMolotov = true;
+    public static final String fireDuration = "tick_durations.fireDuration";
+    public static final String riposteDuration = "tick_durations.riposteDuration";
+    public static final String swordId = "items_ids.sword";
+    public static final String helmId = "items_ids.helm";
+    public static final String chestId = "items_ids.chest";
+    public static final String bootsId = "items_ids.boots";
+    public static final String legsId = "items_ids.legs";
+    public static final String pickId = "items_ids.pick";
+    public static final String axeId = "items_ids.axe";
+    public static final String shovelId = "items_ids.shovel";
+    public static final String hitChance = "success_chances.hit";
+    public static final String smeltChance = "success_chances.smelt";
+
 
 }
