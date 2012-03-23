09dbd820c7251e74ad1ea3f47aa6002629a7e514
JD Davenport <acyrid@gmail.com>
2012-03-22 22:11:13 -0500

    Added configurable hover block

--- src/main/java/com/acyrid/SunSteel/listeners/SSPlayerListener.java
+++ src/main/java/com/acyrid/SunSteel/listeners/SSPlayerListener.java
@@ -1,6 +1,7 @@
 package com.acyrid.SunSteel.listeners;
 
 import com.acyrid.SunSteel.SunSteel;
+import com.acyrid.SunSteel.utils.SSConfig;
 import com.acyrid.SunSteel.utils.SSMechanics;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -18,6 +19,7 @@ public class SSPlayerListener implements Listener{
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event){
         Player player = event.getPlayer();
+        Block waterWalkBlock;
         if(SSMechanics.hasSSHoverWater(player)||SSMechanics.hasSSHoverLava(player)){
             if(!revertSet.containsKey(player)){revertSet.put(player,new HashSet<Block>());}
             Block toBlockLoc = event.getTo().getBlock();
@@ -29,12 +31,12 @@ public class SSPlayerListener implements Listener{
                         Block block = toBlockLoc.getRelative(dx, -1, dz);
                         if(((block.getType() == Material.WATER || block.getType() == Material.STATIONARY_WATER))&&
                                 SSMechanics.hasSSHoverWater(player)){
-                            player.sendBlockChange(block.getLocation() ,Material.GLASS,(byte)0 );
+                            player.sendBlockChange(block.getLocation() ,Material.getMaterial(SSConfig.waterwalkID),(byte)0 );
                             revertSet.get(player).add(block);
                             revertCheck(player, Material.WATER);
                         }else if(((block.getType() == Material.LAVA || block.getType() == Material.STATIONARY_LAVA))&&
                                 SSMechanics.hasSSHoverLava(player)){
-                            player.sendBlockChange(block.getLocation() ,Material.GLASS,(byte)0 );
+                            player.sendBlockChange(block.getLocation() ,Material.getMaterial(SSConfig.lavawalkID),(byte)0 );
                             revertSet.get(player).add(block);
                             revertCheck(player, Material.LAVA);
                         }
